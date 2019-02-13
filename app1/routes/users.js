a584e75209aa26db08dbe1b6b5b5a9689db9997b
Hiroki Arakawa <konbu.and.katsuobushi@gmail.com>
2019-02-13 15:51:47 +0000

    first commit

---
+++ app1/routes/users.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', (req, res) => {
+    const userCollection = req.db.get('userCollection');
+    userCollection.find({}, {}, (e, data) => {
+        console.log(data);
+        res.render('users', {userList: data});
+    });
+});
+
+module.exports = router;
